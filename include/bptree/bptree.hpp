3bb1ed705e8e4eb147bcfdd96fef80a5a92b37cb
Nitesh Tiwari <nktiwari@amazon.com>
2025-09-20 14:08:22 +0530

    fix: include directory missing from CI due to .gitignore pattern
    Fixed .gitignore excluding include/bptree/ and added header files to Git.
    Documentation check should now pass.

---
+++ include/bptree/bptree.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <memory>
+#include <cstdio>
+
+namespace bptree {
+
+class Node {
+    /*
+		Generally size of the this node should be equal to the block size. Which will limit the number of disk access and increase the accesssing time.
+		Intermediate nodes only hold the Tree pointers which is of considerably small size(so they can hold more Tree pointers) and only Leaf nodes hold
+		the data pointer directly to the disc.
+
+		IMPORTANT := All the data has to be present in the leaf node
+	*/
+   public:
+    bool isLeaf;
+    std::vector<int> keys;
+    //Node* ptr2parent; //Pointer to go to parent node CANNOT USE check https://stackoverflow.com/questions/57831014/why-we-are-not-saving-the-parent-pointer-in-b-tree-for-easy-upward-traversal-in
+    Node* ptr2next;              //Pointer to connect next node for leaf nodes
+    union ptr {                  //to make memory efficient Node
+        std::vector<Node*> ptr2Tree;  //Array of pointers to Children sub-trees for intermediate Nodes
+        std::vector<FILE*> dataPtr;   // Data-Pointer for the leaf node
+
+        ptr();   // To remove the error !?
+        ~ptr();  // To remove the error !?
+    } ptr2TreeOrData;
+
+    friend class BPTree;  // to access private members of the Node and hold the encapsulation concept
+   public:
+    Node();
+    ~Node();  // Destructor for proper cleanup
+};
+
+class BPTree {
+    /*
+		::For Root Node :=
+			The root node has, at least two tree pointers
+		::For Internal Nodes:=
+			1. ceil(maxIntChildLimit/2)     <=  #of children <= maxIntChildLimit
+			2. ceil(maxIntChildLimit/2)-1  <=  #of keys     <= maxIntChildLimit -1
+		::For Leaf Nodes :=
+			1. ceil(maxLeafNodeLimit/2)   <=  #of keys     <= maxLeafNodeLimit -1
+	*/
+   private:
+    int maxIntChildLimit;                                   //Limiting  #of children for internal Nodes!
+    int maxLeafNodeLimit;                                   // Limiting #of nodes for leaf Nodes!!!
+    Node* root;                                             //Pointer to the B+ Tree root
+    void insertInternal(int x, Node** cursor, Node** child);  //Insert x from child in cursor(parent)
+    Node** findParent(Node* cursor, Node* child);
+    Node* firstLeftNode(Node* cursor);
+    void destroyTree(Node* node);                          // Helper function for cleanup
+
+   public:
+    BPTree();
+    BPTree(int degreeInternal, int degreeLeaf);
+    ~BPTree();  // Destructor for proper cleanup
+    Node* getRoot();
+    int getMaxIntChildLimit();
+    int getMaxLeafNodeLimit();
+    void setRoot(Node *);
+    void display(Node* cursor);
+    void seqDisplay(Node* cursor);
+    void search(int key);
+    void insert(int key, FILE* filePtr);
+    void removeKey(int key);
+    void removeInternal(int x, Node* cursor, Node* child);
+};
+
+} // namespace bptree
